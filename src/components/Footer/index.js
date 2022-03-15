7e7e65c77f46246642768316a46e6f77edc4eb15
cjdomacena <cjdomacena@yahoo.com>
2022-03-15 19:23:47 -0400

    fix: :bug: Item on local storage wasn't removed when button is clicked

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,9 +1,32 @@
+import { useContext } from "react";
+import { AppContext } from "../../App";
+
 function Banner() {
+	const {setValues} = useContext(AppContext)
+	const defaultValue = {
+		lastUpdated: handleUpdateTime(),
+		daily: {
+			completed: 0,
+			unaDaily: [false, false],
+			guildDono: [false],
+			guardianRaid: [false, false],
+			fieldBoss: [false],
+			rapport: [false],
+			chaosGate: [false],
+		},
+		weekly: {
+			completed: 0,
+			unaWeekly: [false],
+			abyss: [false],
+			ghostShip: [false]
+		}
+	}
 	return(
 		<footer className="w-full bg-[#212121] text-white p-2 text-center text-sm fixed bottom-0">
 			<div className="flex gap-x-2 justify-center items-center">
 				<h1>Note: Checklist is saved on your local storage.</h1>
 				<button className="text-sm underline px-2 py-1" onClick={() => {
+					setValues(defaultValue);
 					localStorage.removeItem('lostArk-todos');
 					alert('Successfully removed from local storage')
 					}}>Remove from local storage</button>
