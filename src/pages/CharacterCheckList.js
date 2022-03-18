f12685bc81a5de477174784a239ca87680199410
cjdomacena <cjdomacena@yahoo.com>
2022-03-18 12:37:25 -0400

    fix: :bug: dependency array fix

--- src/pages/CharacterCheckList.js
+++ src/pages/CharacterCheckList.js
@@ -25,7 +25,7 @@ function CharacterCheckList()
 		setCurrentList(temp[0]);
 		setCurrentOption(abyssalRaids)
 
-	}, [])
+	}, [checkList, id, navigate])
 
 	return (
 		<section className="container mx-auto text-white px-4 mt-12">
